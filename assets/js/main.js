614797d082a439ea1734e6a76d9db64f61c73497
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-05-19 13:51:18 +0100

    - Init Commit. - Create HTML structure in accordance to instructions. - Create Vue.JS structure in accordance to instructions. - Add Methods to addMovie on button click and removeMovie on cross click - Add V-IF to display movies list or empty list message.

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+/**
+ * Create una todo list usando VueJS. Potete dare sfogo alla creativitá e per quanto riguarda l'HTML e il CSS. Se non sapere che fare, di seguito trovate uno screenshot.
+Funzionalitá:
+La nostra todo list avrá alcune tasks di default predefinite
+L'utente puó inserire nuove tasks
+Cliccando sulla "X" l'utente puó cancellare una task
+Se non ci sono piu task nella lista, mostrate un messaggio tipo "Nulla da fare"
+Inoltre L'utente vuole poter modificare una task giá inserita
+ma vuole anche poter indicare che la task é stata completata (con un icona cliccabile)
+Quando l'utente inserisce una task ha due modi per salvarla: o preme il pulsante add o preme il taso Enter della tastiera. Attenzione: l'utente non deve inserire tasks vuote ma almeno un tot di caratteri. */
+
+const root = new Vue({
+    el: "#root",
+
+    data: {
+        newMovie: "",
+        movies: [
+            "Hereditary, regia di Ari Aster",
+            "Ex-Machina, regia di Alex Garland",
+            "Interferenze, regia di Ryan Eslinger"
+        ]
+    },
+
+    methods:{
+        addMovie(){
+            this.movies.push(this.newMovie),
+            this.newMovie = ""
+        },
+
+        removeMovie(index){
+            this.movies.splice(index, 1)
+        }
+    }
+
+})
